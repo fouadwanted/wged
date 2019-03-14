e3fab117478b980b54cd3c8372a3ba0f0a39cc76
fouadwanted <oufaska.fouad@gmail.com>
2019-03-14 09:21:03 +0100

    Ajoutez des fichiers projet.

---
+++ wged/Models/dossier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wged.Models
+{
+    public class dossier
+    {
+        public int Id_dossier { get; set; }
+        public string Nom_dossier { get; set; }
+    }
+}
